6b66061b6b7360b0e16ae5bc6ed9526f0a0cc1a3
casil <casil@SERGIO>
2023-03-26 17:00:58 -0500

    Agregar archivos de proyecto.

---
+++ Taller2Punto1/Circuito.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller2Punto1
+{
+    internal class Circuito
+    { 
+
+            public string Nombre { get; set; }
+            public int Vueltas { get; set; }
+            public Monoplaza MonoplazaEnCircuito { get; private set; }
+
+            public Circuito(string nombre, int vueltas)
+            {
+                Nombre = nombre;     
+                Vueltas = vueltas;
+            }
+
+            public void AgregarMonoplaza(Monoplaza monoplaza)
+            {
+                if (MonoplazaEnCircuito == null)
+                    MonoplazaEnCircuito = monoplaza;
+            }
+
+            public void SacarMonoplaza()
+            {
+                if (MonoplazaEnCircuito != null)
+                    MonoplazaEnCircuito = null;
+            }
+
+            public double RealizarPrueba()
+            {
+                if (MonoplazaEnCircuito == null)
+                    return -1;
+
+                MonoplazaEnCircuito.Encender();
+                MonoplazaEnCircuito.Mover();
+
+                Random random = new Random();
+                double mejorTiempo = double.MaxValue;
+
+                for (int i = 0; i < Vueltas; i++)
+                {
+                double tiempoVuelta = new Random().Next(0, 999999)/1000.0;
+                Console.WriteLine($"Tiempo de vuelta {i + 1}: {tiempoVuelta}");
+                mejorTiempo = Math.Min(mejorTiempo, tiempoVuelta);
+            }
+
+            MonoplazaEnCircuito.Detener();
+                MonoplazaEnCircuito.Apagar();
+
+                return mejorTiempo;
+            }
+        }
+    }
---
+++ Taller2Punto1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller2Punto1
+{
+    internal class Program
+    {
+        
+    static void Main(string[] args)
+        {
+            Circuito circuito = new Circuito("Circuito de pruebas", 5);
+            List<Monoplaza> monoplazas = new List<Monoplaza> { new McLaren(), new Ferrari(), new RedBull() };
+            Dictionary<string, double> resultados = new Dictionary<string, double>();
+               
+            foreach (Monoplaza monoplaza in monoplazas)
+            {
+                circuito.AgregarMonoplaza(monoplaza);
+                double mejorTiempo = circuito.RealizarPrueba();
+                resultados.Add(monoplaza.Escuderia, mejorTiempo);
+                circuito.SacarMonoplaza();
+            }
+
+            Console.WriteLine("\nTabla de posiciones:");
+            foreach (var resultado in resultados)
+            {
+                Console.WriteLine($"{resultado.Key}: {resultado.Value} segundos");
+            }
+        }     
+    }
+    class McLaren : Monoplaza
+    {
+        public McLaren() : base("McLaren") { }
+    }
+
+    class Ferrari : Monoplaza
+    {
+        public Ferrari() : base("Ferrari") { }
+    }
+
+    class RedBull : Monoplaza
+    {
+        public RedBull() : base("RedBull") { }
+    }
+}
+
